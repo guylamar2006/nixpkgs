36e62f3fc720bd2af3a4959cf8a6d03c15d2383b
Guy Chronister <guychronister@outlook.com>
2025-09-20 14:37:40 +0000

    thinkpad-scripts: migrate to by-name and python3Packages
    Refactor package definition to follow by-name and modern Python
    packaging conventions:
    - switch from buildPythonPackage to python3.pkgs.buildPythonPackage
    - drop top-level setuptools argument in favor of python3.pkgs.setuptools
    - update src to use tag + hash instead of rev + sha256
    - replace propagatedBuildInputs with dependencies
    
    This aligns thinkpad-scripts with current Nixpkgs Python packaging
    practices and by-name directory structure.

--- pkgs/by-name/th/thinkpad-scripts/package.nix
+++ pkgs/by-name/th/thinkpad-scripts/package.nix
@@ -1,11 +1,10 @@
 {
   lib,
-  buildPythonPackage,
   fetchFromGitHub,
-  setuptools,
+  python3,
 }:
 
-buildPythonPackage rec {
+python3.pkgs.buildPythonPackage rec {
   pname = "thinkpad-scripts";
   version = "4.12.0";
   format = "setuptools";
@@ -13,11 +12,11 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "martin-ueding";
     repo = "thinkpad-scripts";
-    rev = "v${version}";
-    sha256 = "08adx8r5pwwazbnfahay42l5f203mmvcn2ipz5hg8myqc9jxm2ky";
+    tag = "v${version}";
+    hash = "sha256-foraZWLYV/Rg+TcKy3atAwhXqCBeQeXs+orzWzLqTSE=";
   };
 
-  propagatedBuildInputs = [ setuptools ];
+  dependencies = with python3.pkgs; [ setuptools ];
 
   meta = {
     description = "Screen rotation, docking and other scripts for ThinkPad® X220 and X230 Tablet";
