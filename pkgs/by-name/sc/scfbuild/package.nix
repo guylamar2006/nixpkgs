a76373cdd2c436bbc8be7a80ff6ea431398086aa
Guy Chronister <guychronister@outlook.com>
2025-09-20 14:30:45 +0000

    scfbuild: migrate to by-name and python3Packages
    Refactor package definition to follow by-name conventions:
    - move from buildPythonApplication to python3.pkgs.buildPythonApplication
    - drop top-level python/pyyaml/fonttools/fontforge/setuptools args
    - use python3.pkgs dependencies instead of propagatedBuildInputs
    - update sha256 → hash
    - adjust sitePackages references to python3
    - remove `with lib;` in meta and qualify license with lib.licenses

--- pkgs/by-name/sc/scfbuild/package.nix
+++ pkgs/by-name/sc/scfbuild/package.nix
@@ -1,15 +1,10 @@
 {
   lib,
-  buildPythonApplication,
   fetchFromGitHub,
-  python,
-  pyyaml,
-  fonttools,
-  fontforge,
-  setuptools,
+  python3,
 }:
 
-buildPythonApplication {
+python3.pkgs.buildPythonApplication {
   pname = "scfbuild";
   version = "2.0.0";
 
@@ -19,10 +14,10 @@ buildPythonApplication {
     owner = "13rac1";
     repo = "scfbuild";
     rev = "6d84339512a892972185d894704efa67dd82e87a";
-    sha256 = "0wkyzkhshlax9rvdmn441gv87n9abfr0qqmgs8bkg9kbcjb4bhad";
+    hash = "sha256-TcFFlmRrpjcX0q9iDLJbKtmD9guE2Np2Tl1RqOH8fnI=";
   };
 
-  propagatedBuildInputs = [
+  dependencies = with python3.pkgs; [
     pyyaml
     fonttools
     fontforge
@@ -32,17 +27,17 @@ buildPythonApplication {
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/${python.sitePackages}
-    cp -r scfbuild $out/${python.sitePackages}
+    mkdir -p $out/${python3.sitePackages}
+    cp -r scfbuild $out/${python3.sitePackages}
     cp -r bin $out
 
     runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     description = "SVGinOT color font builder";
     homepage = "https://github.com/13rac1/scfbuild";
-    license = licenses.gpl3;
+    license = lib.licenses.gpl3;
     maintainers = [ ];
     mainProgram = "scfbuild";
   };
