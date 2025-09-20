70ce2e20d66ef2a765935d1bee5503d5bb9ce942
Guy Chronister <guychronister@outlook.com>
2025-09-20 14:24:00 +0000

    scfbuild: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4101,8 +4101,6 @@ with pkgs;
     };
   };
 
-  scfbuild = python3.pkgs.callPackage ../tools/misc/scfbuild { };
-
   segger-jlink-headless = callPackage ../by-name/se/segger-jlink/package.nix { headless = true; };
 
   selectdefaultapplication = libsForQt5.callPackage ../applications/misc/selectdefaultapplication { };
