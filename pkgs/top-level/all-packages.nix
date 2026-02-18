aaa7bea55fec1162b37266a10f154f3ea83e549e
quantenzitrone <general@dev.quantenzitrone.eu>
2026-02-19 00:00:09 +0100

    pulseaudio: move to pkgs/by-name
    this shouldn't create any rebuilds

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8360,8 +8360,6 @@ with pkgs;
 
   hsphfpd = callPackage ../servers/pulseaudio/hsphfpd.nix { };
 
-  pulseaudio = callPackage ../servers/pulseaudio { };
-
   qpaeq = libsForQt5.callPackage ../servers/pulseaudio/qpaeq.nix { };
 
   pulseaudioFull = pulseaudio.override {
